35116a65381492b8f783db2504e431812b35e273
Nurlan Garash <68768916+nurlangarash@users.noreply.github.com>
2021-06-05 22:40:05 +0400

    Update 2044.py

---
+++ 2000-3000/2044.py
@@ -0,0 +1,25 @@
+a = int(input())
+if(a == 1):
+    print("January")
+elif(a == 2):
+    print("February")
+elif(a == 3):
+    print("March")
+elif(a == 4):
+    print("April")
+elif(a == 5):
+    print("May")
+elif(a == 6):
+    print("June")
+elif(a == 7):
+    print("July")
+elif(a == 8):
+    print("August")
+elif(a == 9):
+    print("September")
+elif(a == 10):
+    print("October")
+elif(a == 11):
+    print("November")
+elif(a == 12):
+    print("December")
